7b49cdb211438a4b5ec5a7cd476b76a943f0a2ae
essaathar <122386200+essaathar@users.noreply.github.com>
2023-04-19 19:57:00 +0500

    signup bug fixed

--- app.py
+++ app.py
@@ -5,24 +5,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY']  = 'dceicjewkjj1o3u98549efuo4'
 
 
-@app.route('/admin')
-def admin():
-    conn = sqlite3.connect('students.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM applications")
-    applications = [dict(name=row[0], email=row[1], phone=row[2], address=row[3], major=row[4]) for row in c.fetchall()]
-    conn.close()
-
-    conn = sqlite3.connect('donors.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM donors")
-    donors = [dict(name=row[0], email=row[1], phone=row[2], address=row[3], amount=row[4]) for row in c.fetchall()]
-    conn.close()
-
-    return render_template('admin.html', applications=applications, donors=donors)
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -40,25 +22,45 @@ def home():
         sponsor_cursor.execute('SELECT * FROM sponsors WHERE username=? AND password=?', (username, password))
         sponsor_conn.commit()
         sponsor = sponsor_cursor.fetchone()
+
+        admin_conn = sqlite3.connect('atiya-e-ilm.db')
+        admin_cursor = admin_conn.cursor()
+        admin_cursor.execute('SELECT * FROM admins WHERE username=? AND password=?', (username, password))
+        admin_conn.commit()
+        admin = admin_cursor.fetchone()
+
         # print(f"student: {student}, sponsor: {sponsor}")
         if student:
              return render_template('student_homepage.html', first_name = student[1])
         elif sponsor:
             return redirect(url_for('sponsor_homepage'))
-            # student_funding_conn = sqlite3.connect('student_funding.db')
-            # student_funding_cursor = student_funding_conn.cursor()
-            # student_funding_cursor.execute('SELECT * FROM student_funding')
-            # student_funding = student_funding_cursor.fetchall()
+        elif admin:
+            return redirect(url_for('admin'))
         else:
+            flash('User doesnt exist. Try Again!', category='error')
             return render_template('home.html')
     else:
         return render_template('home.html')
 
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    conn = sqlite3.connect('atiya-e-ilm.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM applications")
+    applications = [dict(name=row[0], email=row[1], phone=row[2], address=row[3], major=row[4]) for row in c.fetchall()]
+    conn.close()
 
+    conn = sqlite3.connect('atiya-e-ilm.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM sponsors")
+    donors = [dict(name=row[0], email=row[1], phone=row[2], address=row[3], amount=row[4]) for row in c.fetchall()]
+    conn.close()
+
+    return render_template('admin.html', applications=applications, donors=donors)
 
 # define route for sponsor signup
-@app.route('/signup/sponsor')
+@app.route('/signup/sponsor', methods=['GET', 'POST'])
 def sponsor_signup():
     if request.method == 'POST':
         # get the form data
@@ -73,7 +75,8 @@ def sponsor_signup():
         InsertSponsor(conn, first_name=first_name, last_name=last_name, 
                       username=username, password=password, email=email, company=company)
         conn.close()
-        return render_template('sponsor_homepage.html', first_name = first_name)
+        return redirect(url_for('sponsor_homepage'))
+        # return render_template('sponsor_homepage.html', first_name = first_name)
     else:
         return render_template('sponsor_signup.html')
 
@@ -100,30 +103,26 @@ def student_signup():
 def account(username):
     return render_template('student_homepage.html', first_name=username)
 
-@app.route('/donation',  methods=['GET', 'POST'])
-def donation():
+
+@app.route('/donate/<student_id>' ,methods=['GET', 'POST'])
+def donation(student_id):
+    # retrieve the row data using the student_id
+    if request.method == 'POST':
+        amount = request.form['amount']
+        conn = sqlite3.connect('atiya-e-ilm.db')
+        cursor = conn.cursor()
+        cursor.execute("UPDATE applications SET amount_collected = amount_collected + ? WHERE student_id = ?", (amount, student_id))
+        conn.commit()
+        conn.close()
+        return render_template('testpage.html')
+
+
+    #print(student_id)
     return render_template('donation.html')
 
-# @app.route('/process_donation', methods=['POST'])
-# def process_donation():
-#     # Retrieve form data
-#     card_number = request.form['card_number']
-#     cardholder_name = request.form['cardholder_name']
-#     expiry_date = request.form['expiry_date']
-#     security_code = request.form['security_code']
-#     amount = request.form['amount']
-    
-#        # Print the form data
-#     print(f"Card Number: {card_number}")
-#     print(f"Cardholder Name: {cardholder_name}")
-#     print(f"Expiry Date: {expiry_date}")
-#     print(f"Security Code: {security_code}")
-#     print(f"Amount: {amount}")
 
 @app.route('/sponsor_homepage',  methods=['GET', 'POST'])
 def sponsor_homepage():
-    if request.method == 'POST':
-        return redirect(url_for('donation'))
     conn = sqlite3.connect('atiya-e-ilm.db')
     cursor = conn.cursor()
     cursor.execute('''
@@ -135,6 +134,7 @@ def sponsor_homepage():
     data = cursor.fetchall()
     # print(data)
     conn.close()
+
     return render_template('sponsor_homepage.html', data=data)
 
 if __name__ == '__main__':
